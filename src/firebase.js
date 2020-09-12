227fc67b8ec5ac443f7747696714c1ce5b0b07f6
Lillian Dixon <dixon.lillian92@gmail.com>
2020-09-12 11:24:57 -0600

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDuzSlF9qMFkJtNX40yf69_hNHEdnjf7OI",
+  authDomain: "clone-de5d1.firebaseapp.com",
+  databaseURL: "https://clone-de5d1.firebaseio.com",
+  projectId: "clone-de5d1",
+  storageBucket: "clone-de5d1.appspot.com",
+  messagingSenderId: "773002936975",
+  appId: "1:773002936975:web:7f08714b64f93d5ab726fa",
+  measurementId: "G-TQJS9BK26W"
+};
