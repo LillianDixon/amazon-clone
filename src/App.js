c6bc6c0965445e0d8acf029c5816e14891fc9c4a
Lillian Dixon <dixon.lillian92@gmail.com>
2020-09-15 16:10:44 -0600

    login page frontend/ready for firebase

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import './App.css';
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
         <Switch>
           <Route path="/login">
-            <h1>Login page</h1>
+            <Login />
           </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path = "/">
+            <Header />
             <Home />
           </Route>
 
